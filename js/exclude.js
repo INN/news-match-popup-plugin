36102e77a51014ed264841fbf35423a8ccfa9a28
Ben Keith <ben@benlk.com>
2017-10-05 17:41:44 -0400

    avoid conflict between elements with the same goshdarn ID by specifiying mailchimp signup forms that are inside a Popup Maker .pum popup

--- js/exclude.js
+++ js/exclude.js
@@ -1,16 +1,13 @@
 (function ($) {
 	$(document).on( 'pumBeforeOpen', function( ) {
-		var $popup = PUM.getPopup('#mc_embed_signup');
-		console.log( $popup );
+		var $popup = PUM.getPopup('.pum #mc_embed_signup');
 
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#Using_special_characters
 		var escaped = news_match_popup_basics_mailchimp.campaign.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-		console.log( escaped );
 		var match = new RegExp( 'utm_source=' + escaped, 'i' );
-		console.log( match );
 
 		if (window.location.href.match( match )) {
-			console.log( $popup.addClass('preventOpen') );
+			$popup.addClass('preventOpen');
 		}
 	});
 }(jQuery));
