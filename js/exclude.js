26fdda47ea2b7fc1353df7a90832e0466a89b1d6
Ben Keith <ben@benlk.com>
2017-10-11 15:40:11 -0400

    Set fallback mailchimp selector to more specific so it only grabs within popups

--- js/exclude.js
+++ js/exclude.js
@@ -9,7 +9,7 @@
 			if ( news_match_popup_basics_mailchimp.selector ) {
 				var $popup = PUM.getPopup( news_match_popup_basics_mailchimp.selector );
 			} else {
-				var $popup = PUM.getPopup( '#mc_embed_signup' );
+				var $popup = PUM.getPopup( '.pum #mc_embed_signup' );
 				console.log( 'Something is wrong with the filter applied to \'news_match_popup_basics_mailchimp_selector\'. Please ensure that it is returning a string and that that string is a valid CSS selector' );
 			}
 
